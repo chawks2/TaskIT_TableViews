cc76712aab991faf2e9d32493846dab5331f9a1e
GEST Precision <Mufasa@BATTLECAT-2.local>
2015-04-09 09:09:20 -0400

    TaskIT - TableView Intro

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,13 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by GEST Precision on 4/1/15.
+//  Copyright (c) 2015 GEST Precision. All rights reserved.
+//
+
+import Cocoa
+
+class TaskCell: UITableViewCell {
+
+}
